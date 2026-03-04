0306f8bf7c6ad7fc865da595fdfbad5d0a2ddb87
AbogiC <venturaabedbogichrist314@gmail.com>
2026-03-04 16:42:52 +0700

    adjust column sheet music

--- backend/db/adjustdb.sql
+++ backend/db/adjustdb.sql
@@ -1,3 +1,6 @@
 ALTER TABLE sheet_music MODIFY COLUMN cover_image LONGTEXT NULL;
 ALTER TABLE sheet_music MODIFY COLUMN description LONGTEXT NULL;
 ALTER TABLE users MODIFY COLUMN role ENUM('user', 'composer', 'admin') DEFAULT 'user';
+ALTER TABLE sheet_music ADD COLUMN created_by INT(11) NOT NULL AFTER views_count;
+
+UPDATE sheet_music SET created_by = 1;
