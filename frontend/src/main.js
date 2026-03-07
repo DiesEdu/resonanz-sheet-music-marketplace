a63a63c49596c312585fa43350388b5a420eec89
AbogiC <venturaabedbogichrist314@gmail.com>
2026-03-07 11:58:48 +0700

    add prevent right click inspect

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,6 +10,10 @@ import router from './router'
 
 const app = createApp(App)
 
+document.addEventListener('contextmenu', function (e) {
+  e.preventDefault()
+})
+
 app.use(createPinia())
 app.use(router)
 
