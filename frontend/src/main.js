7f5ce4acff4f8faf3d942654d8ee0629d189fe36
AbogiC <venturaabedbogichrist314@gmail.com>
2026-03-10 11:34:03 +0700

    (temp) remove x frame options, add restricted inspect

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,9 +10,9 @@ import router from './router'
 
 const app = createApp(App)
 
-// document.addEventListener('contextmenu', function (e) {
-//   e.preventDefault()
-// })
+document.addEventListener('contextmenu', function (e) {
+  e.preventDefault()
+})
 
 app.use(createPinia())
 app.use(router)
